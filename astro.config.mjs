ba7dc341c92a1cac68a6cea2468334d1882e3053
fez7yoe <fez7yoe@gmail.com>
2025-01-30 14:01:54 +0800

    📝 docs: 新增與修改設定檔案

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel()
+  output: 'server', // 輸出模式：static 靜態生成、server 伺服器算繪。
+  adapter: vercel({
+    webAnalytics: { // 網站分析功能：true 開啟、false 關閉。
+        enabled: true,
+    },
+    imageService: true, // 圖片處理功能：true 開啟、false 關閉。
+    devImageService: "sharp", // 在開發中所使用的圖片處理服務。
+  }),
 });
